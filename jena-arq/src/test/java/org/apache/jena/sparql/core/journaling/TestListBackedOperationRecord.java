abe87db58a357e2a08257eff08a02e9dec4e376b
ajs6f <ajs6f@virginia.edu>
2015-07-22 10:31:04 -0400

    Adding license headers

--- jena-arq/src/test/java/org/apache/jena/sparql/core/journaling/TestListBackedOperationRecord.java
+++ jena-arq/src/test/java/org/apache/jena/sparql/core/journaling/TestListBackedOperationRecord.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.jena.sparql.core.journaling;
 
 import static java.util.Arrays.asList;
@@ -33,9 +51,9 @@ public class TestListBackedOperationRecord extends Assert {
 	public void testAdd() {
 		final List<MockOp> results = new ArrayList<>();
 		final ListBackedOperationRecord<MockOp> testRecord = new ListBackedOperationRecord<>(results);
-		testRecord.add(mockOp1);
-		testRecord.add(mockOp2);
-		testRecord.add(mockOp3);
+		testRecord.accept(mockOp1);
+		testRecord.accept(mockOp2);
+		testRecord.accept(mockOp3);
 		assertEquals(3, results.size());
 		assertEquals(mockOp1, results.get(0));
 		assertEquals(mockOp2, results.get(1));
